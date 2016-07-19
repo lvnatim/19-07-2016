3c78e3d0d2b01e3ecfb06241d84256dba5e5a044
Tim Lu <tim.lu@ourlog.ca>
2016-07-19 23:29:12 +0000

    Added benchmark tests for created bubblesort method.

--- bubblesort.rb
+++ bubblesort.rb
@@ -1,3 +1,6 @@
+require 'benchmark'
+
+#bubble sort algorithm
 def sort(arr)
   loop do
     swapped = false
@@ -12,4 +15,38 @@ def sort(arr)
   return arr
 end
 
-puts sort([2, 42, 22, 32, 10, 29, 204])
+def maximum(arr)
+  sort(arr).last
+end
+
+def minimum(arr)
+  sort(arr).first
+end
+
+# Benchmark measures
+print "5000 times sorting with built-in method:"
+puts Benchmark.measure { 5000.times do [2, 42, 22, 02].sort end }
+print "5000 times sorting with created bubble-sort method: "
+puts Benchmark.measure { 5000.times do sort([2, 42, 22, 02]) end }
+
+
+# expect it to return 42 below
+result = maximum([2, 42, 22, 02])
+puts "max of 2, 42, 22, 02 is: #{result}"
+
+# expect it to return 2 below
+result = minimum([2, 42, 22, 02])
+puts "min of 2, 42, 22, 02 is: #{result}"
+
+
+# expect it to return nil when empty array is passed in
+result = maximum([])
+puts "max on empty set is: #{result.inspect}"
+result = minimum([])
+puts "min on empty set is: #{result.inspect}"
+
+result = maximum([-23, 0, -3])
+puts "max of -23, 0, -3 is: #{result}"
+
+result = maximum([6])
+puts "max of just 6 is: #{result}"
